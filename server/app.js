7c7446a27fae79a99574b4cb74322111b9c9e3b4
maxjacobson <max.jacobson@torchlms.com>
2017-09-09 21:43:25 -0600

    Finalize App

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 7575;
 
-app.use('/', express.static('../public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.listen(port, function () {
   console.log('Example app listening on port 7575!')
