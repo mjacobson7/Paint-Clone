98f916716c6c0bd6d7177d36c0d6c8cdeadb3236
maxjacobson <max.jacobson@torchlms.com>
2017-09-09 21:17:48 -0600

    Update Port to unused port on server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use('/', express.static('../public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(7575, function () {
+  console.log('Example app listening on port 7575!')
 })
