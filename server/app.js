585fbec8cab09959ef35ffce386e372107db5e89
maxjacobson <max.jacobson@torchlms.com>
2017-09-09 21:39:59 -0600

    Update Port

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 7575;
 
 app.use('/', express.static('../public'));
 
-app.listen(7575, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 7575!')
 })
